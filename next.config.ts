b799aee76c749db95911ef41af4968075e18a1ad
ArthurTudeng <ardzek@taltech.ee>
2025-08-29 23:11:35 +0300

    Test 5 (Routing considering artteam repo name)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/artteam',
+  assetPrefix: '/artteam/',
   images: {
     unoptimized: true,
   },
